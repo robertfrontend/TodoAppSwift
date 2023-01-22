f753d8b999f1a267d019e6514da5b6b9f63c6893
Robert Romero <robert02frontend@gmail.com>
2023-01-22 08:22:46 -0400

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Robert Romero Medina on 22/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
